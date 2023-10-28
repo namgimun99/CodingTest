31c179ece2420288a53d7ff33830b4f5a3641817
namgimun99 <124684039+namgimun99@users.noreply.github.com>
2023-10-28 18:35:01 +0900

    [Silver III] Title: N과 M (5), Time: 948 ms, Memory: 52628 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[] visit;
+    static int[] ans;
+    static int[] num;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        num = new int[n];
+        ans = new int[m];
+        visit = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i< n; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        NM(0);
+
+        pw.close();
+        br.close();
+
+    }
+
+    public static void NM(int cnt) {
+		if(cnt == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ans.length; i++) {
+                sb.append(ans[i]);
+                if (i < ans.length - 1) {
+                    sb.append(" ");
+                }
+            }
+            System.out.println(sb.toString());
+			return;
+		}
+
+        for(int i = 0; i<num.length; i++) {
+            if(!visit[i]) {
+                ans[cnt] = num[i];
+                visit[i] = true;
+                NM(cnt+1);
+                visit[i] = false;
+            }
+
+        }
+	}
+
+}
