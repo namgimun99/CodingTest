80cef7014e0ccafea82b86434ff041d9d107d503
namgimun99 <124684039+namgimun99@users.noreply.github.com>
2023-11-04 00:13:17 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,45 @@
+
+class Solution {
+    
+    static int count;
+    static int answer;
+    static int sum;
+    static int[] choice;
+    
+    public int solution(int[] nums) {
+        int answer = 0;
+        choice = new int[3];
+        sum = 0;
+
+        answer = nCr(0,0,nums);
+        return answer;
+    }
+    
+    private static int nCr(int cnt,int start,int[] nums) {
+        sum = 0;
+        count = 0;
+
+        if(cnt == 3) {
+            for(int i = 0; i< choice.length; i++){
+                sum += choice[i];
+            }
+            for(int i = 1; i<= sum; i++){
+                if(sum % i == 0){
+                    count++;
+                }
+            }
+
+            if(count <= 2){
+                answer++;
+            }
+            return answer;
+        }
+
+
+        for(int i = start; i<nums.length; i++){
+            choice[cnt] = nums[i];
+            nCr(cnt+1,i+1,nums);
+        }
+        return answer;
+    }
+}
