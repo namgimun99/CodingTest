9f7324fbfffa76c46e45911735dc43f3fa6409c9
남기문 <124684039+namgimun99@users.noreply.github.com>
2025-09-02 22:53:32 +0900

    [Gold V] Title: 용액, Time: 328 ms, Memory: 28580 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[num];
+
+        for(int i = 0 ; i < num; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int right = num -1;
+        int sum = 0;
+        int minResult = Integer.MAX_VALUE;
+        int prevMinResult = 0;
+        int rmbLeft = 0;
+        int rmbRight = 0;
+
+        while(left < right) {
+            sum = arr[left] + arr[right];
+
+            prevMinResult = minResult;
+            minResult = Math.min(minResult,sum < 0 ? sum * -1 : sum);
+
+            if(prevMinResult != minResult) {
+                rmbLeft = left;
+                rmbRight = right;
+            }
+
+            if(sum < 0) {
+                left++;
+            } else if(sum > 0) {
+                right--;
+            } else {
+                rmbLeft = left;
+                rmbRight = right;
+                break;
+            }
+
+
+        }
+
+        System.out.println(arr[rmbLeft] + " " + arr[rmbRight]);
+
+        br.close();
+    }
+}
