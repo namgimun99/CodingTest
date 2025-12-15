ef4505504766989a78db2354ffe4869d103def48
남기문 <124684039+namgimun99@users.noreply.github.com>
2025-12-15 23:58:00 +0900

    [Bronze I] Title: 단어 공부, Time: 244 ms, Memory: 20144 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int[] arr = new int[26]; 
+		String s = br.readLine();
+ 
+		for (int i = 0; i < s.length(); i++){
+ 
+			if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') { 
+				arr[s.charAt(i) - 'A']++;	
+			}
+    
+			else {	// 소문자 범위
+				arr[s.charAt(i) - 'a']++;
+			}
+		}
+ 
+ 
+		int max = -1;
+		char ch = '?';
+ 
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = (char) (i + 65); // 대문자로 출력해야하므로 65를 더해준다.
+			}
+			else if (arr[i] == max) {
+				ch = '?';
+			}
+		}
+ 
+		System.out.print(ch);
+        br.close();
+	}
+ 
+}
